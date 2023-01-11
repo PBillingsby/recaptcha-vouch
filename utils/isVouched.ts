2f6f69b73e7dfb0d1e428439fb86ff239054d0c7
PBillingsby <philbillingsby@gmail.com>
2023-01-11 12:49:13 -0500

    add conditional for isVouch

--- utils/isVouched.ts
+++ utils/isVouched.ts
@@ -5,9 +5,9 @@ export const isVouched = async (address: string) => {
   const contract = warp.contract('_z0ch80z_daDUFqC9jHjfOL8nekJcok4ZRkE_UesYsk')
 
   const { cachedValue }: any = await contract.readState()
-  let res: boolean | undefined
+  let res: any
 
-  if (cachedValue.state.vouched[address]) {
+  if (!!cachedValue.state.vouched[address]) {
     res = cachedValue.state.vouched[address].find((obj: any) => obj.service === "jk0aaivTdKvzeLB_RhpC_ZUoy9CnY2trlEuHQVXulDQ")
   }
 
