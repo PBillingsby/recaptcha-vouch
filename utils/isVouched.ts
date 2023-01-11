001a3ba0d1beb0b1d8d2fe578e1dce4fec62855b
PBillingsby <philbillingsby@gmail.com>
2023-01-11 12:29:30 -0500

    fix for new vouched

--- utils/isVouched.ts
+++ utils/isVouched.ts
@@ -5,7 +5,11 @@ export const isVouched = async (address: string) => {
   const contract = warp.contract('_z0ch80z_daDUFqC9jHjfOL8nekJcok4ZRkE_UesYsk')
 
   const { cachedValue }: any = await contract.readState()
-  const res = cachedValue.state.vouched[address].find((obj: any) => obj.service === "jk0aaivTdKvzeLB_RhpC_ZUoy9CnY2trlEuHQVXulDQ")
+  let res: boolean | undefined
+
+  if (cachedValue.state.vouched[address]) {
+    res = cachedValue.state.vouched[address].find((obj: any) => obj.service === "jk0aaivTdKvzeLB_RhpC_ZUoy9CnY2trlEuHQVXulDQ")
+  }
 
   return res !== undefined
 }
